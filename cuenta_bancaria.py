313c0b5e18ed3259b66e8d6fa183c700503d2f86
Anton Mtz <antonio_halo1@hotmail.com>
2025-05-17 12:24:56 -0600

    Se programo la clase Cliente, se agrego su herencia de Persona y se agregaron sus atributos

--- cuenta_bancaria.py
+++ cuenta_bancaria.py
@@ -2,10 +2,18 @@
 
 class Persona:
 
-    def __init__(self, nombre, apellido):
+    def __init__(self, nombre, apellidos):
         self.nombre = nombre
-        self.apellido = apellido
+        self.apellidos = apellidos
 
-    
+
+class Cliente(Persona):
+
+    def __init__(self, numero_cuenta, balance, nombre, apellidos):
+        super().__init__(nombre, apellidos)
+        self.numero_cuenta = numero_cuenta
+        self.balance = balance
+
+
 
 
