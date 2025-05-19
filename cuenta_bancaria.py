72535275c3f8bb18e035c087370138f66d1940cf
Anton Mtz <antonio_halo1@hotmail.com>
2025-05-19 14:58:55 -0600

    Se programaron los metodos para ingresar los datos del usuario, para validar si los datos son validos y para crear al cliente nuevo

--- cuenta_bancaria.py
+++ cuenta_bancaria.py
@@ -17,7 +17,53 @@ class Cliente(Persona):
     def __str__(self):
         return f"Bienvenido(a) {self.nombre} {self.apellidos}\nSu número de cuenta es: {self.numero_cuenta}\nSu saldo es: {self.balance}"
 
-    
+
+#Funciones
+def inicio():
+
+    print("¡Bienvenido(a)!\nInicia sesión ingresando el número 1.¿Eres un cliente nuevo? Regístrate aquí, ingresa el número 2.\n1.- Iniciar sesión\n2.- Registrarse")
+    opcion_ingresada = input()
+
+    if opcion_ingresada == 1:
+        nombre,apellido = datos_ingresados()
+        validacion,nombre_validado,apellido_validado = validar_datos(nombre, apellido)
+        nuevo_cliente(validacion, nombre_validado, apellido_validado)
+
+
+
+def datos_ingresados():
+
+    nombre = input("Ingresa tu nombre: ")
+    apellidos = input("Ingresa tus apellidos")
+
+    return nombre, apellidos
+
+
+def validar_datos(validar_nombre, validar_apellidos):
+
+    for letra in validar_nombre:
+        if letra.isdigit():
+            return False, validar_nombre, validar_apellidos
+
+    for letra in validar_apellidos:
+        if letra.isdigit():
+            return False, validar_nombre, validar_apellidos
+
+    return True, validar_nombre, validar_apellidos
+
+
+def nuevo_cliente(validacion, nombre, apellidos):
+    if validacion is True:
+        cliente_nuevo = Cliente()
+        return
+    else:
+        return "ERROR. El nombre ingresado o su apellido no es válido, favor de ingresar sus datos nuevamente."
+
+
+
+
+
+
 
 
 
